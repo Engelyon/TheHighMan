d92699f1efbc4015cd00f8a2875c063061fcee11
Allan Engel <hq.allan@gmail.com>
2025-11-06 00:32:23 -0300

    Initial commit

---
+++ src/main/java/thehighman/util/Sounds.java
@@ -0,0 +1,8 @@
+package thehighman.util;
+
+import static thehighman.BasicMod.audioPath;
+
+public class Sounds {
+    public static String TEST_SOUND = audioPath("test.wav"); //Load audio using a given path
+    public static String ding; //Load audio from audioPath based on the field name
+}
