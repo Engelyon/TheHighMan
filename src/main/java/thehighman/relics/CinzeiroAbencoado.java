1b75b595e602a30851f5c7d5c7b9d8f15a67fbcc
Djhemison Machado <35597793+djhemison@users.noreply.github.com>
2025-11-06 11:08:54 -0300

    Reliquias?
    So um teste viado

---
+++ src/main/java/thehighman/relics/CinzeiroAbencoado.java
@@ -0,0 +1,14 @@
+package thehighman.relics;
+
+import com.megacrit.cardcrawl.cards.AbstractCard;
+
+public class CinzeiroAbencoado extends BaseRelic {
+    private static final String NAME = "CinzeiroAbencoado"; //The name will be used for determining the image file as well as the ID.
+    public static final String ID = makeID(NAME); //This adds the mod's prefix to the relic ID, resulting in modID:MyRelic
+    private static final RelicTier RARITY = RelicTier.COMMON; //The relic's rarity.
+    private static final LandingSound SOUND = LandingSound.CLINK; //The sound played when the relic is clicked.
+
+    public CinzeiroAbencoado() {
+        super(ID, NAME, RARITY, SOUND);
+    }
+}
