9719583051e8b54c3eb9c7ad4f2fa68644e3cf92
Allan Engel <hq.allan@gmail.com>
2025-11-11 20:17:51 -0300

    aaa

--- src/main/java/thehighman/cards/Burgao.java
+++ src/main/java/thehighman/cards/Burgao.java
@@ -64,7 +64,7 @@ public class Burgao extends BaseCard {
     public void upgrade() {
         if (!upgraded) {
             upgradeName();
-            refreshDescription();
+            initializeDescription();
         }
     }
 }
