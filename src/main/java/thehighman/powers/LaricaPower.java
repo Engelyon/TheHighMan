cd7971d794fb3a95aee8cb43f5bac9aee0f3325f
Allan Engel <hq.allan@gmail.com>
2025-11-12 15:54:21 -0300

    Update LaricaPower.java

--- src/main/java/thehighman/powers/LaricaPower.java
+++ src/main/java/thehighman/powers/LaricaPower.java
@@ -12,6 +12,7 @@ import static thehighman.InimigosDoSpire.makeID;
 public class LaricaPower extends BasePower {
     public static final String POWER_ID = makeID("LaricaPower");
     private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    private int appliedStacks = 0;
 
     public LaricaPower(AbstractCreature owner, AbstractCreature source, int amount) {
         super(POWER_ID, PowerType.DEBUFF, false, owner, source, amount);
@@ -24,14 +25,38 @@ public class LaricaPower extends BasePower {
         this(owner, owner, amount);
     }
 
-    /*@Override
+    @Override
     public void onInitialApplication() {
-       /AbstractDungeon.actionManager.addToBottom(
-                new ApplyPowerAction(owner, owner, new StrengthPower(owner, 2 * amount), 2 * amount)
-        );
+        if (this.amount > 0) {
+            AbstractDungeon.actionManager.addToBottom(
+                    new ApplyPowerAction(owner, owner, new StrengthPower(owner, 2 * this.amount), 2 * this.amount)
+            );
+            this.appliedStacks = this.amount;
+        }
+        this.updateDescription();
     }
-*/
 
+    @Override
+    public void stackPower(int stackAmount) {
+        if (stackAmount == 0) return;
+        AbstractDungeon.actionManager.addToBottom(
+                new ApplyPowerAction(owner, owner, new StrengthPower(owner, 2 * stackAmount), 2 * stackAmount)
+        );
+        this.amount += stackAmount;
+        this.appliedStacks += stackAmount;
+        if (this.appliedStacks < 0) this.appliedStacks = 0; // segurança
+        this.updateDescription();
+    }
+
+    @Override
+    public void onRemove() {
+        if (this.appliedStacks > 0) {
+            AbstractDungeon.actionManager.addToBottom(
+                    new ApplyPowerAction(owner, owner, new StrengthPower(owner, -2 * this.appliedStacks), -2 * this.appliedStacks)
+            );
+            this.appliedStacks = 0;
+        }
+    }
 
     @Override
     public float atDamageReceive(float damage, DamageInfo.DamageType damageType) {
@@ -42,29 +67,7 @@ public class LaricaPower extends BasePower {
     }
 
     @Override
-    public void onRemove() {
-        AbstractDungeon.actionManager.addToBottom(
-                new ApplyPowerAction(owner, owner, new StrengthPower(owner, -2 * amount), -2 * amount)
-        );
-    }
-
-    @Override
-    public void stackPower(int stackAmount) {
-        if (stackAmount > 0) {
-            AbstractDungeon.actionManager.addToBottom(
-                    new ApplyPowerAction(owner, owner, new StrengthPower(owner, 2 * stackAmount), 2 * stackAmount)
-            );
-        } else if (stackAmount < 0) {
-            AbstractDungeon.actionManager.addToBottom(
-                    new ApplyPowerAction(owner, owner, new StrengthPower(owner, 2 * stackAmount), 2 * stackAmount)
-            );
-        }
-        this.amount += stackAmount;
-        this.updateDescription();
-    }
-
-    @Override
     public void updateDescription() {
-        this.description = powerStrings.DESCRIPTIONS[0].replace("!M!", Integer.toString(amount*3));
+        this.description = powerStrings.DESCRIPTIONS[0].replace("!M!", Integer.toString(amount * 3));
     }
 }
