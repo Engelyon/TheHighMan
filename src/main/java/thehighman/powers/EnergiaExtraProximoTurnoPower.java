b800e7af9925c0c38c279e63afa5b7e1853df071
Allan Engel <hq.allan@gmail.com>
2025-11-12 16:26:19 -0300

    energiaextra

--- src/main/java/thehighman/powers/EnergiaExtraProximoTurnoPower.java
+++ src/main/java/thehighman/powers/EnergiaExtraProximoTurnoPower.java
@@ -21,7 +21,7 @@ public class EnergiaExtraProximoTurnoPower extends AbstractPower {
     }
     @Override
     public void updateDescription() {
-        description = DESCRIPTIONS[0];
+                description = DESCRIPTIONS[0];
     }
 
     @Override
