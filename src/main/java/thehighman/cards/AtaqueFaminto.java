ce20cba6163e6565bb7136f012c52d9041d3c93f
Allan Engel <hq.allan@gmail.com>
2025-11-12 23:46:10 -0300

    Update AtaqueFaminto.java

--- src/main/java/thehighman/cards/AtaqueFaminto.java
+++ src/main/java/thehighman/cards/AtaqueFaminto.java
@@ -2,6 +2,7 @@ package thehighman.cards;
 
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
@@ -35,16 +36,15 @@ public class AtaqueFaminto extends BaseCard {
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        // Primeiro ataque
         addToBot(new DamageAction(m, new DamageInfo(p, this.damage, DamageInfo.DamageType.NORMAL),
                 AbstractGameAction.AttackEffect.BLUNT_LIGHT));
-
-        // Verifica e consome Comido para repetir o ataque
-        if (ComidoPower.consumirComido(p)) {
+        if(p.hasPower(ComidoPower.POWER_ID)){
+            addToBot(new ReducePowerAction(p,p,ComidoPower.POWER_ID,1));
             addToBot(new DamageAction(m, new DamageInfo(p, this.damage, DamageInfo.DamageType.NORMAL),
                     AbstractGameAction.AttackEffect.BLUNT_LIGHT));
         }
     }
+    
     @Override
     public void upgrade() {
         if (!upgraded) {
