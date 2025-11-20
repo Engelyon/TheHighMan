a8f57320c03f9d198f402d264b184499d2de3dd7
Djhemison Machado <35597793+djhemison@users.noreply.github.com>
2025-11-20 17:24:04 -0300

    inspira

--- src/main/java/thehighman/cards/Inspiracao.java
+++ src/main/java/thehighman/cards/Inspiracao.java
@@ -35,7 +35,7 @@ public class Inspiracao extends BaseCard {
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        addToBot(new GainBlockAction(p, BLOCK));
+        addToBot(new GainBlockAction(p, this.block));
         addToBot(new ApplyPowerAction(p, p, new ErvaPower(p,this.magicNumber), this.magicNumber));
         addToBot(new ApplyPowerAction(p, p, new SedaPower(p,p,1),1));
     }
